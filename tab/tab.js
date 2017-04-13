1bf532e6f09783496560d747069c4c550c038aef
AlanWenhao <alan.zhang@juwai.com>
2017-04-13 18:18:12 +0800

    tab

---
+++ tab/tab.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    function Tab(options){
+
+        this.options = {
+            target: '.js-tabs'
+        }
+
+    }
+    
+    Tab.prototype.init = function init(){
+        this.
+    }
+
+});
